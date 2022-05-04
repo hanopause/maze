78465b274c62be117260459690d6581596f84de2
hanopause <93599475+hanopause@users.noreply.github.com>
2022-05-04 22:04:35 +0800

    Add files via upload

---
+++ maze/create.h
@@ -0,0 +1,9 @@
+#ifndef _CREATE_H_
+#define _CREATE_H_
+#include <string>
+#include <vector>
+void mazeInitialize();
+void mazeCreation(int i, int j);
+void spawnSelection();
+std::vector<std::string> getMaze();
+#endif // _CREATE_H_
