539ab0ae6635ad18b82822f3a70896f6b4cd5aec
hanopause <93599475+hanopause@users.noreply.github.com>
2022-07-14 21:08:30 +0800

    Delete maze directory

--- maze/create.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CREATE_H_
-#define _CREATE_H_
-#include <string>
-#include <vector>
-void mazeInitialize();
-void mazeCreation(int i, int j);
-void spawnSelection();
-std::vector<std::string> getMaze();
-#endif // _CREATE_H_
